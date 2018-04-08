e1430421bc3d77e0bc74c16adcb871ebbb34f4c4
absarrahman <raman040635@gmail.com>
2018-04-08 15:10:55 +0000

    Find sum of series ie 1,2,3,4.......
    Thank me later

---
+++ sum_of_series.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+include<conio.h>
+int main()
+{
+    int i,n,s=0;
+    printf("Enter value");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+       s=s+i;
+    printf("sum=%d",s);
+    getch();
+}
